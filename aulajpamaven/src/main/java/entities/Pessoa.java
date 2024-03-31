f76c650abc45b3414f7a7b1e2115b8e3961b5837
douglasmcl <douglasmacielaraujo@outlook.com>
2024-03-31 15:24:28 -0300

    add the persisting maps with JPA

--- aulajpamaven/src/main/java/entities/Pessoa.java
+++ aulajpamaven/src/main/java/entities/Pessoa.java
@@ -2,12 +2,23 @@ package entities;
 
 import java.io.Serializable;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Pessoa implements Serializable {
 	
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
 	private String name;
+	
+	//@Column(name="nomecompleto")
 	private String email;
 	
 	public Pessoa(Integer id, String name, String email) {
